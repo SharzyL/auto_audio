7867a5930c17561558451468a880fe3242762dcf
Sharzy <sharzyluo@outlook.com>
2019-05-04 16:58:02 +0800

    1.0 version

--- server.py
+++ server.py
@@ -4,35 +4,64 @@ import os
 from os.path import join
 import configparser
 
-default_song_dir = 'songs/'
 app = Flask(__name__)
+
+default_song_dir = 'songs/'
+MUSIC_EXT = ['.mp3', '.wav', '.flac', '.wma', '.aac']  # 接受的音乐文件后缀
 config = configparser.ConfigParser()
 config.read('setting.ini')
-setting = config['app'] # setting is a dict to record all config
+setting = config['app']  # setting is a dict to record all config
 
 
 def write_config():
+    """
+    向配置文件写入
+    :return: None
+    """
     with open('setting.ini', 'w') as cfg:
         config.write(cfg)
 
 
 def filter_dir(_dir):
+    """
+    os.path 要求文件夹不以斜线开头，故需要去掉url请求里面的开头斜线
+    :param _dir: 
+    :return: None
+    """
     return _dir[1:] if _dir.startswith('/') or _dir.startswith('\\') else _dir
 
 
 @app.route('/app')
-def index():
+def application():
+    """
+    返回播放器界面
+    :return: 
+    """
     return app.send_static_file('html/index.html')
 
 
 @app.route('/songinfo')
-def songs():
+def songinfo():
+    """
+    返回目录中的歌曲信息，无参数时读取配置文件中指定的目录中的音乐信息
+    当请求url中包含dir参数时返回该目录下的音乐信息
+    返回格式为json，层级如下
+    { 目录名 : [音乐文件名]}
+    
+    :return: json
+    """
     def scan_music(path):
-        MUSIC_EXT = ['.mp3', '.wav', '.flac', '.wma', '.aac']
+        """
+        返回path目录下的所有音乐文件路径（包含path）
+        :return: [str]
+        """
         return [join(path, song) for song in os.listdir(path)
-                if os.path.splitext(song)[1].lower() in MUSIC_EXT]
+                if os.path.splitext(song)[1].lower() in MUSIC_EXT]  # 过滤文件后缀
 
     def get_songs(_dir):
+        """
+        返回将转化为包含音乐信息的dict，形式与待转化的json相同
+        """
         _dir = filter_dir(_dir)
         return {session: scan_music(join(_dir, session)) for session in os.listdir(_dir)
                 if os.path.isdir(join(_dir, session))}
@@ -51,19 +80,33 @@ def songs():
 
 @app.route('/test')
 def test():
-    return send_file('C:\\_dev\\py\\auto_backup\\src\\perm.png')
+    """
+    仅用于功能测试，无作用
+    """
+    return 'test'
 
 
 @app.route('/songs/<session>/<song>')
 def get_song(session, song):
-    path = join(default_song_dir, session, song)
+    """
+    在配置中'song dir'配置项所制定的目录中的获取音乐文件（过滤含'..'的非法请求）
+    """
+    path = join(setting['song dir'], session, song)
     if '..' in path:
         abort(404)
     return send_file(path, as_attachment=True, conditional=True)
 
 
+# TODO 修改为post方法
 @app.route('/setting')
 def edit_setting():
+    """
+    通过url进行设置
+    url参数   作用
+    songDir  修改'song dir'配置项
+    
+    :return: 配置成功的目录名
+    """
     song_dir = filter_dir(request.args['songDir'])
     if os.path.isdir(song_dir):
         setting['song dir'] = song_dir
@@ -72,8 +115,13 @@ def edit_setting():
     else:
         return f'Error 400: Directory "{song_dir}" not exist', 400
 
+
 @app.route('/setting/update')
 def update_setting():
+    """
+    重新读取配置文件
+    """
+    global setting
     config.read('setting.ini')
     setting = config
     print(setting)
--- static/js/index.js
+++ static/js/index.js
@@ -1,41 +1,68 @@
-/* eslint-disable no-console */
+// TODO: 设置界面的样式需要改善
+// TODO: 出错时需要一个对话框
+// TODO: 兼容Edge
+// TODO: 界面中文化
 
-const defaultTime = {
-    'morning': {
-        'hours': 7,
-        'minutes': 40
-    },
-    'noon': {
-        'hours': 14,
-        'minutes': 20
+
+// 快速创建DOM对象并append到paren上
+function createElement(tag, id, className, parent, type, attr_obj) {
+    let ele = document.createElement(tag)
+    if (id) {
+        ele.id = id
     }
+    if (className) {
+        ele.className = className
+    }
+    if (type) {
+        ele.type = type
+    }
+    if (parent) {
+        parent.appendChild(ele)
+    }
+    if (attr_obj) {
+        for (let attr in attr_obj) {
+            ele.setAttribute(attr, attr_obj[attr])
+        }
+    }
+    return ele
 }
 
-window.onload = () => {
-    var playlist = document.getElementById('playlist')
-    var player = document.getElementById('cplayer')
-    var controlBtn = document.getElementById('control-btn')
-    var songDirInput = document.getElementById('song-dir-input')
-    var songVolumeInput = document.getElementsByClassName('cplayer-volume')[0]
+//将文字append到parent上
+function appendText(text, parent) {
+    let ele = document.createTextNode(text)
+    parent.appendChild(ele)
+}
 
+
+window.onload = () => {
+    //储存所有schedule的列表
+    var scheduleList = []
+    let playlist = document.getElementById('playlist')
+    let player = document.getElementById('cplayer')
+    let controlBtn = document.getElementById('control-btn')
+    let songDirInput = document.getElementById('song-dir-input')
+    let songVolumeInput = document.getElementsByClassName('cplayer-volume')[0]
+
+    //从目录中截取音乐名(不含后缀)
     function getSongName(songDir) {
         return /[^/\\]+(?=.mp3$)/.exec(songDir)[0]
     }
 
-
+    //根据传入的songLine对象播放对应的音乐
     function playSongLine(songLine) {
-        player.audio.src = songLine.id
+        player.audio.src = songLine.src
         player.play()
-        player.currentLine = songLine
+        player.currentLine = songLine //储存当前播放的songLine
         playingIcon.play(songLine)
     }
 
 
+    //根据传入的session对象(或session名称)列表播放其中的音乐
     function playSession(session) {
         if (session.constructor == String) {
             session = document.getElementById('session-' + session)
         }
-        var songLines = session.getElementsByClassName('song-line')
+        let songLines = session.getElementsByClassName('song-line')
         playSongLine(songLines[0])
         let count = 1 // record number of played songs
         let num = songLines.length
@@ -47,9 +74,10 @@ window.onload = () => {
         })
     }
 
-
-    var playingIcon = document.createElement('i')
-    playingIcon.id = 'playing-icon'
+    //创建一个用于表示播放状态的图标，它会出现在正在播放的songLine的开头位置
+    //当开始播放音乐时调用它的play方法，如果含songLine对象作为参数则会转移到这个songLine上
+    //暂停时调用pause方法
+    let playingIcon = createElement('i', 'playing-icon')
     player.addEventListener('pause', () => playingIcon.pause())
     player.addEventListener('play', () => playingIcon.play())
     playingIcon.play = (songLine) => {
@@ -61,80 +89,26 @@ window.onload = () => {
     playingIcon.pause = () => {
         playingIcon.className = 'fa fa-pause'
     }
-    playingIcon.hide = () => {
-        playingIcon.style.display = 'none'
-    }
 
 
-    // fill playlist
-    function renderPlaylist(songs) {
-        for (let session in songs) {
-            var sessionObj = document.createElement('div')
-            sessionObj.className = 'session'
-            sessionObj.id = 'session-' + session
-            playlist.appendChild(sessionObj)
-
-            var headObj = document.createElement('p')
-            headObj.className = 'session-head'
-            sessionObj.appendChild(headObj)
-
-            var sessionNameObj = document.createElement('span')
-            sessionNameObj.innerHTML = session
-            sessionNameObj.className = 'session-name'
-            sessionNameObj.addEventListener('click', () => playSession(session))
-            headObj.appendChild(sessionNameObj)
-
-            var leftTime = document.createElement('span')
-            leftTime.className = 'left-time'
-            leftTime.style.display = 'none'
-            headObj.appendChild(leftTime)
-
-            var songBox = document.createElement('div')
-            songBox.className = 'song-box'
-            sessionObj.appendChild(songBox)
-
-            for (let songDir of songs[session]) {
-                let songLine = document.createElement('p')
-                songLine.innerHTML = getSongName(songDir)
-                songLine.className = 'song-line'
-                songLine.id = songDir
-                songLine.addEventListener('click', () => {
-                    if (songLine == player.currentLine) {
-                        player.toggle()
-                    } else {
-                        playSongLine(songLine)
-                    }
-                })
-                songBox.appendChild(songLine)
-            }
-        }
-    }
-
-    var scheduleList
-    controlBtn.addEventListener('click', () => {
-        scheduleList.forEach((task) => task.toggle())
-        if (scheduleList.isWorking) {
-            controlBtn.innerHTML = '<i class="fa fa-play"></i> Start Auto Play'
-            controlBtn.className = 'btn-start'
-        } else {
-            controlBtn.innerHTML = '<i class="fa fa-stop"></i> Stop Auto Play'
-            controlBtn.className = 'btn-stop'
-        }
-        scheduleList.isWorking = !scheduleList.isWorking
-    })
-
-
+    //一个表示每日定时任务的类
     class ScheduleTask {
         constructor(hours, minutes, task) {
+            //hours, minutes表示执行任务的时间，task为要执行的任务(无参函数)
+            //当hours, minutes中有空值时，这个类不工作，但不报错，可以通过reset重设时间来使它开始工作
             this.hours = hours
             this.minutes = minutes
             this.task = task
             this.isWorking = false
-            this.expectedTime = this.getExpectedTime()
+            this.expectedTime = this._getExpectedTime()
         }
 
-        getExpectedTime() {
-            var expectedTime = new Date
+        //内部方法，用于获取下次执行的时间
+        _getExpectedTime() {
+            if (!this.hours || !this.minutes) {
+                return undefined
+            }
+            let expectedTime = new Date
             expectedTime.setHours(this.hours)
             expectedTime.setMinutes(this.minutes)
             expectedTime.setSeconds(0)
@@ -145,24 +119,34 @@ window.onload = () => {
             return expectedTime
         }
 
-        formattedLeftTime() { // return formatted string of the time until next execution
-            var time = this.expectedTime - (new Date)
-            var intDivide = (a, b) => [Math.floor(a / b), a % b]
-            var hours, minutes, seconds;
+        //返回格式化的下次执行的时间，格式形如 HH:MM:SS
+        formattedLeftTime() {
+            if (!this.expectedTime) {
+                return undefined
+            }
+            let time = this.expectedTime - (new Date)
+            //辅助函数，用于执行带余除法，返回[商，余数]数组
+            let intDivide = (a, b) => [Math.floor(a / b), a % b]
+            let hours, minutes, seconds;
             [hours, time] = intDivide(time, 3600 * 1000);
             [minutes, time] = intDivide(time, 60 * 1000);
             [seconds, time] = intDivide(time, 1000);
             [hours, minutes, seconds] = [hours, minutes, seconds].map(String).map((str) => {
                 if (str.length < 2) {
                     str = '0' + str
-                }
+                } //将每个参数都转换为长为2的字符串
                 return str
             })
             return hours + ':' + minutes + ':' + seconds
         }
 
+        // 开始倒计时执行任务
         start() {
-            this.countdown = this.getExpectedTime() - (new Date)
+            if (!this.expectedTime) {
+                this.isWorking = true
+                return
+            }
+            this.countdown = this._getExpectedTime() - (new Date) //表示离下次执行所剩的时间
             this.timeOutHandler = setTimeout(() => {
                 this.task()
                 this.expectedTime.setDate(this.expectedTime.getDate() + 1)
@@ -176,6 +160,7 @@ window.onload = () => {
             this.isWorking = true
         }
 
+        //暂停执行任务
         cancel() {
             if (this.timeOutHandler) {
                 clearTimeout(this.timeOutHandler)
@@ -186,6 +171,7 @@ window.onload = () => {
             this.isWorking = false
         }
 
+        //切换准备执行/暂停执行状态
         toggle() {
             if (this.isWorking) {
                 this.cancel()
@@ -193,49 +179,181 @@ window.onload = () => {
                 this.start()
             }
         }
+
+        //重新设定时间
+        reset(hours, minutes) {
+            this.hours = hours
+            this.minutes = minutes
+            this.expectedTime = this._getExpectedTime()
+            if (this.isWorking) {
+                this.cancel()
+                this.start()
+            }
+        }
     }
 
 
+    // 表示定时播放音乐的类，重写了父类的start, cancel方法
     class ScheduleMusicSession extends ScheduleTask {
         constructor(hours, minutes, session) {
             super(hours, minutes, () => {
                 playSession(session)
             })
             this.sessionObj = document.getElementById('session-' + session)
+            this.playTimeObj = this.sessionObj.getElementsByClassName('play-time')[0]
             this.leftTimeObj = this.sessionObj.getElementsByClassName('left-time')[0]
         }
 
         start() {
             super.start()
-            this.leftTimeObj.style.display = 'inline'
+            this.leftTimeObj.style.visibility = 'visible'
             this.display()
             this.flushHandler = setInterval(() => this.display(), 1000)
         }
 
+        //在LeftTimeObj上显示剩余时间
         display() {
-            this.leftTimeObj.innerHTML = this.formattedLeftTime() + ' later'
+            if (!this.expectedTime) {
+                this.leftTimeObj.innerHTML = 'Unspecified playing time' //当类不工作时显示提示文字
+            } else {
+                this.leftTimeObj.innerHTML = this.formattedLeftTime() + ' later'
+            }
         }
 
         cancel() {
             super.cancel()
-            this.leftTimeObj.style.display = 'none'
+            this.leftTimeObj.style.visibility = 'hidden'
             clearInterval(this.flushHandler)
         }
     }
 
 
-    var setting = {
-        'defaultDir': '/songs',
-        'defaultTime': {
-            'morning': {
-                'hours': 7,
-                'minutes': 40
-            },
-            'noon': {
-                'hours': 14,
-                'minutes': 20
+    //header上的controlBtn用于控制所有schedule的开关
+    controlBtn.addEventListener('click', () => {
+        scheduleList.forEach((task) => task.toggle())
+        if (scheduleList.isWorking) {
+            controlBtn.innerHTML = '<i class="fa fa-play"></i> Start Auto Play'
+            controlBtn.className = 'btn-start'
+        } else {
+            controlBtn.innerHTML = '<i class="fa fa-stop"></i> Stop Auto Play'
+            controlBtn.className = 'btn-stop'
+        }
+        scheduleList.isWorking = !scheduleList.isWorking
+    })
+
+
+    // 根据传入的songs参数来创建歌曲列表
+    //songs的格式形如{session name: [full song path]}
+    // TODO: 在服务器提取metadata然后再songLine中分栏显示
+    function renderPlaylist(songs) {
+        for (let session in songs) {
+
+            //创建DOM对象
+            let sessionObj = createElement('div', 'session-' + session, 'session', playlist)
+            let headObj = createElement('p', null, 'session-head', sessionObj)
+            let sessionNameObj = createElement('span', null, 'session-name', headObj)
+            sessionNameObj.innerHTML = session
+            sessionNameObj.addEventListener('click', () => playSession(session)) //单击session文字时列表播放session
+
+
+            let playTimeInput = createElement('div', null, 'play-time', headObj)
+            appendText('Scheduled At ', playTimeInput)
+            let hourInput = createElement('input', null, 'time-input', playTimeInput, 'text', {
+                'maxlength': '2'
+            })
+            appendText(':', playTimeInput)
+            let minuteInput = createElement('input', null, 'time-input', playTimeInput, 'text', {
+                'maxlength': '2'
+            })
+            let leftTime = createElement('span', null, 'left-time', headObj)
+            leftTime.style.visibility = 'hidden'
+
+            // 如果localStorage中有缓存的话，则将缓存内容写入输入框
+            let hours = localStorage.getItem(session + '-h')
+            let minutes = localStorage.getItem(session + '-m')
+            if (hours) {
+                hourInput.value = hours
             }
-        },
+            if (minutes) {
+                minuteInput.value = minutes
+            }
+
+            let schedule = new ScheduleMusicSession(hours, minutes, session)
+            scheduleList.push(schedule);
+            [hourInput.legal, minuteInput.legal] = [true, true] //legal成员变量用于表示输入框中的内容是否合法(是否为范围内的整数)
+
+            //当hourInput, minuteInput每次输入时判断输入是否合法，不合法时输入框变为红色
+            hourInput.addEventListener('input', () => {
+                if (/^\d*$/.test(hourInput.value) && hourInput.value <= 23) {
+                    hourInput.style.backgroundColor = '#bfcff2'
+                    hourInput.style.boxShadow = 'none'
+                    hourInput.legal = true
+                } else {
+                    hourInput.style.backgroundColor = '#ffb3b3'
+                    hourInput.style.boxShadow = 'inset 0px 0px 3px #ff3333'
+                    hourInput.legal = false
+                }
+            })
+            minuteInput.addEventListener('input', () => {
+                if (/^\d*$/.test(minuteInput.value) && minuteInput.value <= 59) {
+                    minuteInput.style.backgroundColor = '#bfcff2'
+                    minuteInput.style.boxShadow = 'none'
+                    minuteInput.legal = true
+                } else {
+                    minuteInput.style.backgroundColor = '#ffb3b3'
+                    minuteInput.style.boxShadow = 'inset 0px 0px 3px #ff3333'
+                    minuteInput.legal = false
+                }
+            })
+
+            //当两个输入框的输入均为合法时，重设schedule对象，并将输入缓存进localStorage中
+            //若有不合法的输入框，则将输入框重设为localStorage中缓存的值
+            var submitTime = () => {
+                if (hourInput.legal && minuteInput.legal) {
+                    var hours = hourInput.value
+                    var minutes = minuteInput.value
+                    if (hours && minutes) {
+                        localStorage.setItem(session + '-h', hours)
+                        localStorage.setItem(session + '-m', minutes)
+                    }
+                    schedule.reset(hours, minutes)
+                } else {
+                    if (!hourInput.legal) {
+                        hourInput.value = localStorage.getItem(session + '-h')
+                    }
+                    if (!minuteInput.legal) {
+                        minuteInput.value = localStorage.getItem(session + '-m')
+                    }
+                }
+            }
+            hourInput.addEventListener('change', submitTime)
+            minuteInput.addEventListener('change', submitTime)
+
+
+            //生成该session内的播放列表
+            let songBox = createElement('div', null, 'song-box', sessionObj)
+            for (let songDir of songs[session]) {
+                let songLine = createElement('p', null, 'song-line', songBox)
+                songLine.innerHTML = getSongName(songDir)
+                songLine.src = songDir
+                songLine.addEventListener('click', () => {
+                    if (songLine == player.currentLine) {
+                        player.toggle()
+                    } else {
+                        playSongLine(songLine)
+                    }
+                })
+            }
+        }
+    }
+
+
+
+    //用于操作localStorage中储存的配置的对象
+    //目前支持songDir(音乐目录) 和volume(默认音量)的存储
+    //当读取setting中的值时，若localStorage中有存储则调用该存储，否则调用默认值
+    let setting = {
+        'defaultDir': '/songs',
         'defaultVolume': 50,
 
         get songDir() {
@@ -247,8 +365,11 @@ window.onload = () => {
             }
         },
 
+        //每次写入时向服务器验证是否为合法目录，非法时服务器返回400，此时讲输入框变红，并且不向localStorage中写入
+        //合法时需要刷新页面以应用更新
+        //TODO: 在非法时显示提示信息
         set songDir(dir) {
-            var dirChange = new XMLHttpRequest
+            let dirChange = new XMLHttpRequest
             dirChange.open('get', '/setting?songDir=' + encodeURI(dir))
             dirChange.onreadystatechange = () => {
                 if (dirChange.readyState == XMLHttpRequest.DONE) {
@@ -266,6 +387,7 @@ window.onload = () => {
             dirChange.send(null)
         },
 
+        //注意setting中获取的音量是以100作为最大值的
         get volume() {
             if (localStorage.getItem('volume')) {
                 return localStorage.getItem('volume')
@@ -290,7 +412,8 @@ window.onload = () => {
     })
 
 
-    var songsRequest = new XMLHttpRequest
+    //向服务器请求songinfo，然后调用renderPlayList渲染
+    let songsRequest = new XMLHttpRequest
     songsRequest.open('get', '/songinfo')
     songsRequest.setRequestHeader('Content-Type', 'application/json;charset=utf-8')
     songsRequest.onreadystatechange = function () {
@@ -298,15 +421,11 @@ window.onload = () => {
             return
         }
         if (songsRequest.status != 200) {
-            console.log('Error ' + songsRequest.status + ': Failed to load song info')
+            // console.log('Error ' + songsRequest.status + ': Failed to load song info')
             return
         }
-        var response = JSON.parse(songsRequest.responseText)
+        let response = JSON.parse(songsRequest.responseText)
         renderPlaylist(response)
-        scheduleList = [
-            new ScheduleMusicSession(7, 30, 'morning'),
-            new ScheduleMusicSession(20, 4, 'noon')
-        ]
     }
     songsRequest.send(null)
 }
